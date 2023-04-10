5d2e42f4a1449694500e1730c135745d625a35a8
root <root@DESKTOP-IBECEG1>
2023-04-11 02:13:42 +0300

    fix link,

--- installation.sh
+++ installation.sh
@@ -2,9 +2,9 @@
 mkdir -p ~/.kuvartitmp
 cd ~/.kuvartitmp
 
-curl -s http://kuvarti.vousso.com/config.sh		--output config.sh
-curl -s http://kuvarti.vousso.com/.vimrc		--output .vimrc
-curl -s http://kuvarti.vousso.com/.zshrc		--output .zshrc
+curl -s http://kuvarti.vousso.com/myconfig/config.sh	--output config.sh
+curl -s http://kuvarti.vousso.com/myconfig/.vimrc		--output .vimrc
+curl -s http://kuvarti.vousso.com/myconfig/.zshrc		--output .zshrc
 
 sh config.sh
 cd .. && rm -rf .kuvartitmp
