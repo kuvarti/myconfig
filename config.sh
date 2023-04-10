407b21e8dfcb73c618813de31f630bd63709cc37
root <root@DESKTOP-IBECEG1>
2023-04-11 00:09:25 +0300

    first settings

---
+++ config.sh
@@ -0,0 +1,15 @@
+# configration for vimrc
+
+if [ -z "$(cat ~/.zshrc | grep vim=\'nvim\')" ]
+then
+	echo "alias vim='nvim'" >> ~/.zshrc
+fi
+mkdir -p ~/.config/nvim
+if [ -f ~/.config/nvim/init.vim ]
+then
+	rm ~/.config/nvim/init.vim
+fi
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+
+cp ~/.vimrc ./vimrc.background
+cp ./.vimrc ~/.vimrc
