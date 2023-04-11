f90d1ab3a8a9686dfaf2d58ded25d76b949ad54e
Kuvarti <uzali.kafasi@gmail.com>
2023-04-11 17:40:48 +0300

    new refresh

--- refresh.sh
+++ refresh.sh
@@ -1,2 +1,4 @@
-cd /home/kuvartis/kuvarti.vousso.com/custom && git pull && cd ..
-cp custom/refresh.sh refresh.sh
+cd /home/kuvartis/kuvarti.vousso.com/custom && git pull
+chmod o+x $(ls -a | grep -v .git | tail -n +3)
+chmod o+r $(ls -a | grep -v .git | tail -n +3)
+cd .. && cp custom/refresh.sh refresh.sh
