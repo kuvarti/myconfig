451ba78dc32594b943182136537e90bf523605a1
Kuvarti <uzali.kafasi@gmail.com>
2023-04-11 16:51:01 +0300

    finish ?

--- refresh.sh
+++ refresh.sh
@@ -1,11 +1,2 @@
-cd /home/kuvartis/kuvarti.vousso.com/custom
-if [ -z "$(git pull | grep 'Already up to date.')" ]
-then
-	cd /home/kuvartis/kuvarti.vousso.com
-	rm -rf myconfig
-	cp -r custom myconfig
-	chmod o+r myconfig
-	chmod o+r $(find myconfig)
-	chmod o+x $(find myconfig)
-fi
+cd /home/kuvartis/kuvarti.vousso.com/custom && git pull && cd ..
 cp custom/refresh.sh refresh.sh
