e8cbad8d2d27ca407cd48ba797493bfc61f701c8
Kuvarti <uzali.kafasi@gmail.com>
2023-04-11 16:44:09 +0300

    fix privileges

--- refresh.sh
+++ refresh.sh
@@ -4,6 +4,8 @@ then
 	cd /home/kuvartis/kuvarti.vousso.com
 	rm -rf myconfig
 	cp -r custom myconfig
+	chmod o+r myconfig
 	chmod o+r $(find myconfig)
+	chmod o+x $(find myconfig)
 fi
 cp custom/refresh.sh refresh.sh
